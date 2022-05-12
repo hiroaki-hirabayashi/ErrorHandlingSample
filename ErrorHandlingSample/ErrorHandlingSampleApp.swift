5cc81d36c08dcd08a60d47d8ac4cbcd8204e9871
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-05-12 21:47:45 +0900

    雛形作成、.gitignore追加

--- ErrorHandlingSample/ErrorHandlingSampleApp.swift
+++ ErrorHandlingSample/ErrorHandlingSampleApp.swift
@@ -14,7 +14,7 @@ struct ErrorHandlingSampleApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
---
+++ ErrorHandlingSample/Test/TestErrorView.swift
@@ -0,0 +1,20 @@
+//
+//  TestErrorView.swift
+//  ErrorHandlingSample
+//
+//  Created by Hiroaki-Hirabayashi on 2022/05/12.
+//
+
+import SwiftUI
+
+struct TestErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestErrorView()
+    }
+}
